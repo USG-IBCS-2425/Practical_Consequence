0005a6e1ab80e6c595a799cf66b6086ce3037057
Abigail Chen <abigail_chen@m018752.local>
2024-09-06 16:00:59 -0700

    Pset1 HW

---
+++ Pset1.java
@@ -0,0 +1,9 @@
+class Pset1 {
+
+	public static void main(String[] args) {
+		System.out.println("Abigail Chen " + "is a student in IB CS");
+		System.out.println("Python is an interpreted language that executes code line by line. On the other hand, Java is a compiled language that compiles code into bytecode.");
+		System.out.println("High level languages are easier to understand and are more widely used today. High level language is the abstraction of Low Level Langauge. Assembly language is a type of low level language that communicates eith the computers hardware directly");
+		System.out.println("To translate high level langauge that we use to low level langauge for the computers to udnerstand");	
+	}
+}
